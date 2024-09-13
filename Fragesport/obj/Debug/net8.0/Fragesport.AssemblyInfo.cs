c9fe00df0128fbf4e8902bc3cc88a0743b431aa0
Mostafa Nouri <mostafa.nouri@elev.ga.ntig.se>
2024-09-13 14:52:09 +0200

    jag är stolt över detta

--- Fragesport/obj/Debug/net8.0/Fragesport.AssemblyInfo.cs
+++ Fragesport/obj/Debug/net8.0/Fragesport.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fragesport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+577acc5e0ef41d7eee412d51831111d9eea78813")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fragesport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fragesport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
